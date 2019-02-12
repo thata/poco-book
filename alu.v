1440d85a99e4fbe639eb3aaf09fee70d8df4748b
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2019-02-12 22:44:31 +0900

    aluへの司令を定数化した

--- alu.v
+++ alu.v
@@ -1,22 +1,28 @@
+`define ALU_THA 3'b000
+`define ALU_THB 3'b001
+`define ALU_AND 3'b010
+`define ALU_OR  3'b011
+`define ALU_SL  3'b100
+`define ALU_SR  3'b101
+`define ALU_ADD 3'b110
+`define ALU_SUB 3'b111
+
 module alu(
     input [15:0] a, b,
     input [2:0] com,
-    output [15:0] y);
+    output reg [15:0] y);
     
-    reg [15:0] y_reg;
-
-    assign y = y_reg;
-
     always @(*) begin
         case(com)
-            3'b000: y_reg <= a;
-            3'b001: y_reg <= b;
-            3'b010: y_reg <= a & b;
-            3'b011: y_reg <= a | b;
-            3'b100: y_reg <= a << 1;
-            3'b101: y_reg <= a >> 1;
-            3'b110: y_reg <= a + b;
-            default: y_reg <= a - b;
+            `ALU_THA: y = a;
+            `ALU_THB: y = b;
+            `ALU_AND: y = a & b;
+            `ALU_OR:  y = a | b;
+            `ALU_SL:  y = a << 1;
+            `ALU_SR:  y = a >> 1;
+            `ALU_ADD: y = a + b;
+            `ALU_SUB: y = a - b;
+            default:  y = 16'bzzzzzzzzzzzzzzzz;
         endcase
     end
 endmodule
